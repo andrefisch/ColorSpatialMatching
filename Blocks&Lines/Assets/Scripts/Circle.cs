bf0948837bb7a6188ba8eb4fc35c9ab7be9b4ee9
Andrew Fischl <anfischl@gmail.com>
2017-01-14 20:00:30 -0500

    Added a script to create a growing circle around a block and attached it to default block

---
+++ Blocks&Lines/Assets/Scripts/Circle.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Circle : MonoBehaviour
+{
+    public int segments;
+    public float radius;
+    LineRenderer line;
+    Color color;
+
+    void Start ()
+    {
+        color = gameObject.GetComponent<SpriteRenderer>().color;
+        line = gameObject.GetComponent<LineRenderer>();
+        line.SetColors(color, color);
+        line.SetVertexCount (segments + 1);
+        line.useWorldSpace = false;
+        CreatePoints ();
+    }
+
+    void Update()
+    {
+        bool DEUPDATE = false;
+        if (radius < 4)
+        {
+            radius += 0.13f;
+        }
+        else if (color.a > 0)
+        {
+            color.a -= 0.04f;
+            if (DEUPDATE)
+            {
+                Debug.Log("Start color is: " + color.a);
+            }
+            line.SetColors(color, color);
+        }
+
+        line.SetWidth(0.3f, 0.3f);
+        line.SetPosition(0, Vector3.zero);
+        line.SetPosition(1, Vector3.up);
+        Material whiteDiffuseMat = new Material(Shader.Find("Particles/Additive (Soft)"));
+        line.material = whiteDiffuseMat;
+        CreatePoints();
+    }
+
+
+    void CreatePoints ()
+    {
+        float x;
+        float y;
+        float z = 0f;
+
+        float angle = 0f;
+
+        for (int i = 0; i < (segments + 1); i++)
+        {
+            x = Mathf.Sin (Mathf.Deg2Rad * angle);
+            y = Mathf.Cos (Mathf.Deg2Rad * angle);
+            line.SetPosition (i, new Vector3(x, y, z) * radius);
+            angle += (360f / segments);
+        }
+    }
+}
