dd3a80488fab27721ead43531ccdc2ee9638bec7
Andrew Fischl <anfischl@gmail.com>
2017-07-28 23:06:56 -0400

    added musical stuff

---
+++ Blocks&Lines/Assets/Scripts/AudioSourceController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSourceController : MonoBehaviour {
+
+    public AudioSource audioSource;
+    public AudioClip[] notes;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    public void PlayNote(int baseNote, int combo)
+    {
+        int max = notes.Length;
+        int note = baseNote + combo - 2;
+        if (note < max)
+        {
+            audioSource.PlayOneShot(notes[note]);
+        }
+        else
+        {
+            audioSource.PlayOneShot(notes[max - 1]);
+        }
+    }
+}
